d2e1345032f84359ded825803c2554440c441ec2
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2025-01-15 21:11:28 +0000

    updated data generation logic to include the markscheme, and updated data.json accordingly.

--- interfaces/ai_tutor/generate_data.py
+++ interfaces/ai_tutor/generate_data.py
@@ -7,7 +7,8 @@ from helpers import load_questions_and_answers
 
 def generate_question(question, markscheme, marks, idx):
     data = dict()
-    data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), f"data_{idx}")
+    data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+    data_path = os.path.join(data_dir, f"data_{idx}")
     generation_client = unify.Unify("o1@openai", cache=True)
     targets = dict()
     for target in range(marks + 1):
@@ -39,6 +40,8 @@ def generate_question(question, markscheme, marks, idx):
 
 def combine_data():
     data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
+    if os.path.exists(os.path.join(data_dir, "data.json")):
+        os.remove(os.path.join(data_dir, "data.json"))
     data_paths = [os.path.join(data_dir, fname) for fname in os.listdir(data_dir)]
     data = {}
     for data_path in data_paths:
@@ -58,7 +61,7 @@ def main():
     markschemes = [dct["answer"] for dct in ans_n_marks]
     marks = [dct["marks"] for dct in ans_n_marks]
     idxs = range(num_questions)
-    unify.map(generate_question, questions, markschemes, marks, idxs)
+    # unify.map(generate_question, questions, markschemes, marks, idxs)
     combine_data()
 
 
