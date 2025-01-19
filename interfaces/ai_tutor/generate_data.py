060c1853ed2337faa2c271b83068461d4d2fce06
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2025-01-19 18:05:09 +0000

    simplified data generation script.

--- interfaces/ai_tutor/generate_data.py
+++ interfaces/ai_tutor/generate_data.py
@@ -15,17 +15,13 @@ args = parser.parse_args()
 mode = "usage" if args.usage else "labelled"
 
 
-def generate_question(
-        question, markscheme, available_marks, subject, paper_id, question_num,
-        question_imgs, markscheme_imgs, idx
-):
-    data = dict()
+def generate_question(question, data, idx):
     data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
     fname = f"{mode}_data_{idx}"
     data_path = os.path.join(data_dir, fname)
     generation_client = unify.Unify("o1@openai", cache=True)
     targets = dict()
-    for target in range(available_marks + 1):
+    for target in range(data["marks"] + 1):
         response = generation_client.generate(
             system_message=GENERATE_RESPONSE_PROMPT.replace(
                 "{target}",
@@ -33,7 +29,7 @@ def generate_question(
             )
             .replace(
                 "{num_marks}",
-                str(available_marks),
+                str(data["marks"]),
             )
             .replace(
                 "{question}",
@@ -41,17 +37,24 @@ def generate_question(
             )
             .replace(
                 "{markscheme}",
-                markscheme,
+                data["answer"],
             ),
         )
+        breakpoint()
         targets[target] = [ans for ans in response.split("Answer:")[1:]]
-    targets["subject"] = subject
-    targets["paper_id"] = paper_id
-    targets["question_num"] = question_num
-    targets["markscheme"] = markscheme
-    targets["available_marks"] = available_marks
-    targets["question_imgs"] = question_imgs
-    targets["markscheme_imgs"] = markscheme_imgs
+    targets["subject"] = data["subject"]
+    targets["paper_id"] = data["paper_id"]
+    targets["question_num"] = data["question_num"]
+    targets["markscheme"] = data["markscheme"]
+    targets["available_marks"] = data["available_marks"]
+    if data["question_imgs"]:
+        targets["question_imgs"] = [fp.split("/")[-1] for fp in data["question_imgs"]]
+    else:
+        targets["question_imgs"] = None
+    if data["markscheme_imgs"]:
+        targets["markscheme_imgs"] = [fp.split("/")[-1] for fp in data["markscheme_imgs"]]
+    else:
+        targets["markscheme_imgs"] = None
     # incremental file writing
     data[question] = targets
     with open(data_path, "w+") as f:
@@ -78,30 +81,8 @@ def combine_data():
 
 def main():
     qna = load_questions_and_answers()
-    questions = list(qna.keys())
-    ans_n_marks = list(qna.values())
-    num_questions = len(questions)
-    assert num_questions == len(ans_n_marks)
-    markschemes = [dct["answer"] for dct in ans_n_marks]
-    available_marks = [dct["marks"] for dct in ans_n_marks]
-    subjects = [dct["subject"] for dct in ans_n_marks]
-    paper_ids = [dct["paper_id"] for dct in ans_n_marks]
-    question_nums = [dct["question_num"] for dct in ans_n_marks]
-    question_imgs = [
-        [fp.split("/")[-1] for fp in dct["question_imgs"]]
-        if dct["question_imgs"] else None
-        for dct in ans_n_marks
-    ]
-    markscheme_imgs = [
-        [fp.split("/")[-1] for fp in dct["markscheme_imgs"]]
-        if dct["markscheme_imgs"] else None
-        for dct in ans_n_marks
-    ]
-    idxs = range(num_questions)
-    unify.map(
-        generate_question, questions, markschemes, available_marks, subjects, paper_ids,
-        question_nums, question_imgs, markscheme_imgs, idxs
-    )
+    args = [(question, dct, i) for i, (question, dct) in enumerate(qna.items())]
+    unify.map(generate_question, args, mode="loop")
     combine_data()
 
 
