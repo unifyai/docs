b93c87ee8fe508f6d1cdc3220475568c0f279cbe
djl11 <dan@unify.ai>
2025-02-23 00:33:34 +0700

    added trace view nested img.

--- demo_testing/logs_trace_view.py
+++ demo_testing/logs_trace_view.py
@@ -1,8 +1,20 @@
 import unify
-unify.activate("traced-view-demo")
+unify.activate("nested-traced-view-demo")
 
 @unify.traced
-def my_function(a, b):
+def inner_fn(a, b):
     return a + b
 
-my_function(1, 2)
+@unify.traced
+def mid_fn(a, b):
+    c = inner_fn(a, b)
+    d = inner_fn(c, b)
+    return c * d
+
+@unify.traced
+def outer_fn(a, b):
+    c = mid_fn(a, b)
+    d = mid_fn(c, a)
+    return d / c
+
+outer_fn(3, 4)
