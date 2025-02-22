0d5e7c6ed972eb7e407749af8a0f2eb4cf0cb75c
djl11 <dan@unify.ai>
2025-02-23 04:16:43 +0700

    added scatter two groups gif.

--- demo_testing/derived_entry.py
+++ demo_testing/derived_entry.py
@@ -5,10 +5,10 @@ random.seed(0)
 unify.activate("derived_entry", overwrite=True)
 
 for _ in range(20):
-    x = random.random()
-    y = random.random()
+    x = random.randint(0, 100)
+    y = random.randint(0, 100)
     unify.log(
         x=x,
         y=y,
-        length=(x**2 + y**2)**0.5
+        # length=(x**2 + y**2)**0.5
     )
