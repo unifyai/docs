35fe4e236aea829e16fbbfcea8eb4044993406e4
vedpatwardhan <vedpat3@gmail.com>
2026-01-14 11:57:22 +0530

    purged many more files

--- demo_testing/derived_columns_demo.py
+++
@@ -1,7 +0,0 @@
-import unify
-import random
-
-unify.activate("derived-columns-demo", overwrite=True)
-
-for _ in range(20):
-    unify.log(x=random.random(), y=random.random())
--- demo_testing/global_filter_demo.py
+++
@@ -1,7 +0,0 @@
-import unify
-
-unify.activate("global-filter-demo", overwrite=True)
-unify.log(x=-1, y=-1)
-unify.log(x=-1, y=1)
-unify.log(x=1, y=-1)
-unify.log(x=1, y=1)
